81029ecb20aa1494b48e347fded687ca479430ca
shubhamkumar96 <shubhamkumarranu@gmail.com>
2022-06-22 02:35:12 +0530

    Added Service Layer

---
+++ internal/comment/comment.go
@@ -0,0 +1,42 @@
+package comment
+
+import (
+	"context"
+	"fmt"
+)
+
+//	Comment - a representation of the comment structure for our service
+type Comment struct {
+	ID     string
+	Slug   string
+	Body   string
+	Author string
+}
+
+//	Store - Interface for Repository Layer
+type Store interface {
+	GetComment(context.Context, string) (Comment, error)
+}
+
+//	Service - is the struct on which all our logic will be built on top of
+type Service struct {
+	Store Store
+}
+
+//	NewService - returns a pointer to a new Service
+func NewService(store Store) *Service {
+	return &Service{
+		Store: store,
+	}
+}
+
+//	GetComment - Method to Fetch Comment based on ID
+func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
+	fmt.Println("Retrieving a Comment")
+	cmt, err := s.Store.GetComment(ctx, id)
+	if err != nil {
+		fmt.Println(err)
+		return Comment{}, err
+	}
+	return cmt, nil
+}
