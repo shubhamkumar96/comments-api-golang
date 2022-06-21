a9feec3d92a7217df13f9a154365a0911284890a
shubhamkumar96 <shubhamkumarranu@gmail.com>
2022-06-22 02:59:34 +0530

    Added Stub Methods and Error handling

--- internal/comment/comment.go
+++ internal/comment/comment.go
@@ -2,7 +2,14 @@ package comment
 
 import (
 	"context"
+	"errors"
 	"fmt"
+)
+
+//	Define Error Variables
+var (
+	ErrFetchingComment = errors.New("failed to fetch comment by id")
+	ErrNotImplemented  = errors.New("not implemented")
 )
 
 //	Comment - a representation of the comment structure for our service
@@ -14,6 +21,7 @@ type Comment struct {
 }
 
 //	Store - Interface for Repository Layer
+//	Defines all the methods that our service needs in order to operate
 type Store interface {
 	GetComment(context.Context, string) (Comment, error)
 }
@@ -36,7 +44,19 @@ func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
 	cmt, err := s.Store.GetComment(ctx, id)
 	if err != nil {
 		fmt.Println(err)
-		return Comment{}, err
+		return Comment{}, ErrFetchingComment
 	}
 	return cmt, nil
+}
+
+func (s *Service) UpdateComment(ctx context.Context, cmt Comment) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) DeleteComment(ctx context.Context, id string) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) CreateComment(ctx context.Context, cmt Comment) (Comment, error) {
+	return Comment{}, ErrNotImplemented
 }
